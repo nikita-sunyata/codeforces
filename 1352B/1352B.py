ea2a5a55af6e048262493d6b277e90b236bc4477
nikita-sunyata <nikitasunyata@gmail.com>
2020-12-04 09:44:00 +0800

    1352B add comments

--- 1352B/1352B.py
+++ 1352B/1352B.py
@@ -9,14 +9,18 @@ for test in range(int(input())):
         print('YES')
         print(' '.join( [ str(n//k) ] * k ))
 
+    #if the remainder % 2 == 0 , means that we can first have k  "n//k"s , then you add the remainder to the first one  
     elif (n%k) % 2 == 0:
         print('YES')
-        print(  
-            '{}'.format((n//k)+(n%k)),' '.join([str(n//k)]*(k-1)) 
-            )
+        print( '{}'.format((n//k)+(n%k)),' '.join([str(n//k)]*(k-1)) )
     
     elif(n%k) % 2 != 0:
 
+        # so first is that we set to have k "n//k-1" (achiving the fact that we have to have k numbers)
+        # then we still have to add the rest of the number ( n - ( k * ((n//k)-1) to the first number
+        # of course we have to first check if the rest of the number will be able to "%2=0"
+        # else the parity won't be the same
+        # finally we also have to first check if ((n//k)-1) > 0 , since you need every element > 0.
         if ((((n - ((n//k)-1)*k)) % 2 ) == 0 ) and ((n//k)-1)>0 :
 
             print('YES')
