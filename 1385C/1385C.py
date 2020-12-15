24cc3814ee080e4ebb0da1b5f5059aad822a8de7
nikita-sunyata <nikitasunyata@gmail.com>
2020-12-15 20:01:24 +0800

    1385C update (unsolved)

---
+++ 1385C/1385C.py
@@ -0,0 +1,16 @@
+for test in range(int(input())):
+    
+    list_len = int(input())
+    data = [int(x) for x in input().split(' ')]
+    start_index = 0
+    end_index = list_len # cuz [start:end],so we don't need to -1
+    deleted = 0
+    test_list = data[start_index:end_index]
+    for i in data:
+        if max()
+
+
+    def check(a_list):
+        start_index = 0
+        end_index = len(a_list)
+        for i in a_list[]
